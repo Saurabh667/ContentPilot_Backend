4fb6d0bf54ddab4fbd3619d82c458267b0b2cc7c
Saurabh667 <saurav000667@gmail.com>
2026-04-05 12:54:25 +0530

    initial backend push

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class User(models.Model):
+    uid = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    photo = models.URLField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
---
+++ users/views.py
@@ -0,0 +1,28 @@
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import User
+
+@csrf_exempt
+def save_user(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        uid = data.get("uid")
+        name = data.get("name")
+        email = data.get("email")
+        photo = data.get("photo")
+
+        user, created = User.objects.get_or_create(
+            uid=uid,
+            defaults={
+                "name": name,
+                "email": email,
+                "photo": photo,
+            }
+        )
+
+        return JsonResponse({
+            "message": "User saved",
+            "created": created
+        })
